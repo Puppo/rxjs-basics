3d4a2c00f47677348084391bd439f8a84ce206a6
Luca Del Puppo <luca.delpuppo@aton.com>
2019-08-14 15:53:57 +0200

    counter with scan operators

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import style from "./css/main.css";
+
+import { interval } from 'rxjs';
+import { scan, mapTo, filter, tap } from 'rxjs/operators';
+
+// elms ref
+const countdown = document.getElementById('countdown');
+const message = document.getElementById('message');
+
+
+const counter$ = interval(1000).pipe(
+    mapTo(-1),
+    scan((accumulator, current) => accumulator + current, 10),
+    tap({
+        next: console.log,
+        complete: () => console.log('complete')
+    }),
+    filter(value => value >= 0)
+);
+
+counter$.subscribe({
+    next: value => {
+        countdown.innerHTML = value;
+        if (!value) {
+            message.innerHTML = 'Loftoff!'
+        }
+    }
+});
