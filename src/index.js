87d0e51f9d019d8979fe94990a788e4f3838e80f
Luca Del Puppo <luca.delpuppo@aton.com>
2019-08-14 14:58:25 +0200

    Lab 1 - progressive bar with document scroll

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import style from "./css/main.css";
+
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+function calculateScrollPercent(element) {
+    const {
+        scrollTop,
+        scrollHeight,
+        clientHeight
+    } = element;
+
+    return (scrollTop / (scrollHeight - clientHeight)) * 100;
+}
+
+// elems
+const progressBar  = document.querySelector('.progress-bar');
+
+const scroll$ = fromEvent(document, 'scroll');
+const progress$ = scroll$.pipe(
+    // percent progress
+    map(({target}) => calculateScrollPercent(target.documentElement))
+);
+
+
+progress$.subscribe({
+    next: percent => progressBar.style.width = `${percent}%`
+});
