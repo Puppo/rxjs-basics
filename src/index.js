e49185f3209bb7687c955b6f35ceb784b575cffc
Luca Del Puppo <luca.delpuppo@aton.com>
2019-08-31 14:52:11 +0200

    Create http polling example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import style from "./css/main.css";
+
+import { fromEvent, timer } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { takeUntil, mergeMapTo, finalize, tap, switchMap, switchMapTo, pluck, exhaustMap } from 'rxjs/operators';
+
+
+// elems
+const  startButton = document.getElementById('start');
+const  stopButton = document.getElementById('stop');
+const  pollingStatus = document.getElementById('polling-status');
+const  dogImg = document.getElementById('dog');
+
+// streams
+const startClick$ = fromEvent(startButton, 'click');
+
+const stopClick$ = fromEvent(stopButton, 'click');
+
+startClick$.pipe(
+    exhaustMap(() => timer(0, 5000).pipe(
+        tap(() => pollingStatus.innerHTML = 'Active'),
+        switchMapTo(
+            ajax.getJSON('https://random.dog/woof.json').pipe(
+                pluck('url')
+            )
+        ),
+        takeUntil(stopClick$),
+        finalize(() => pollingStatus.innerHTML = 'Stopped'),
+        )
+    )
+)
+.subscribe({
+    next: url => dogImg.src = url
+})
